f28d7550d92b4825640c9a55bf8b7d5c71577b59
andrei <andrei.i.ivanov@commandus.com>
2017-09-08 12:50:15 +0900

    Add new icons

--- app/src/main/java/com/commandus/buynshare/MealCardAddActivity.java
+++ app/src/main/java/com/commandus/buynshare/MealCardAddActivity.java
@@ -5,9 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class MealCardAddActivity extends AppCompatActivity {
+
+    private static final String TAG = MealCardAddActivity.class.getSimpleName();
+    private AutoCompleteTextView mMealCN;
+    private EditText mEtCost;
+    private EditText mEtQty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +30,57 @@ public class MealCardAddActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                save();
             }
         });
+
+        mMealCN = (AutoCompleteTextView) findViewById(R.id.actv_meal_card_add_cn);
+        mEtCost = (EditText) findViewById(R.id.et_meal_card_add_cost);
+        mEtQty = (EditText) findViewById(R.id.et_meal_card_add_qty);
+        // mEtQty.setText("1");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_meal_card_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.action_add_meal_card:
+                save();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void save() {
+        String meal = "";
+        long cost = 0;
+        int qty = 1;
+        try {
+            meal = mMealCN.getText().toString();
+        } catch (Exception e)
+        {
+            Log.e(TAG, e.toString());
+        }
+        try {
+            qty = Integer.parseInt(mEtQty.getText().toString());
+        } catch (Exception e)
+        {
+            Log.e(TAG, e.toString());
+        }
+        try {
+            cost = Long.parseLong(mEtCost.getText().toString());
+        } catch (Exception e)
+        {
+            Log.e(TAG, e.toString());
+        }
+        // TODO
+        finish();
     }
 }
